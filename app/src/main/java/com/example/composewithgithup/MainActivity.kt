a440ebeffb93bb8aec96ebb2d3392aa5c4c3a9e1
Husanboy <husanboyroziqov7@gmail.com>
2023-02-28 08:41:10 +0300

    Commit Second

--- app/src/main/java/com/example/composewithgithup/MainActivity.kt
+++ app/src/main/java/com/example/composewithgithup/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composewithgithup.ui.theme.ComposeWIthGITHUPTheme
 
@@ -31,6 +36,10 @@ fun Headere() {
           topBar = { TopAppBar (
             title = {  Text(text = "ComposeWith_git_hup", modifier = Modifier.fillMaxWidth(), fontSize = 20.sp, fontFamily = FontFamily.Serif, textAlign = TextAlign.Center)}
             , backgroundColor = colorResource(id =R.color.ColorAppBar )
+          , navigationIcon = { Icon(
+                  imageVector = Icons.Default.MoreVert,
+                  contentDescription = "",
+              )}
 
           ) }
         ) {
